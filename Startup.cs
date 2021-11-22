8856da409f1277c49242773bcc8d777f9c9227de
Maklolaj <m.zelek@op.pl>
2021-11-22 22:22:13 +0100

    CORS policy

--- Startup.cs
+++ Startup.cs
@@ -31,11 +31,19 @@ namespace LabManAPI
         {
 
             services.InstallServicesInAssembly(Configuration);
+            services.AddCors();
+            services.AddMvc(options => options.EnableEndpointRouting = false);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCors(builder => builder
+                   .AllowAnyOrigin()
+                   .AllowAnyMethod()
+                   .AllowAnyHeader());
+            app.UseMvc();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
