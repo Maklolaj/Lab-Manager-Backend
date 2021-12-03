ca7821ef9fd379de911c7ce6f5656dcc4fa0a927
Maklolaj <m.zelek@op.pl>
2021-12-03 14:54:21 +0100

    Fault model

---
+++ Models/Fault.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
+namespace LabManAPI.Models
+{
+    public class Fault
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public Item Item { get; set; }
+
+        public IdentityUser User { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime reportTIme { get; set; }
+
+    }
+}
