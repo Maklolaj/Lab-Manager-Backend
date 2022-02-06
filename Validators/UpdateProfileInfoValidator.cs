20f4cd71f4bac5b32522c0cf4b8fe9c1223aaec7
Maklolaj <m.zelek@op.pl>
2022-02-06 20:36:44 +0100

    add validators

--- Validators/UpdateProfileInfoValidator.cs
+++ Validators/UpdateProfileInfoValidator.cs
@@ -4,16 +4,27 @@ using LabManAPI.Contracts.Requests;
 
 namespace LabManAPI.Validators
 {
-    public class UserUpdateProfileInfoValidator : AbstractValidator<UserUpdateProfileInfo>
+    public class UserChangeEmailInfoValidator : AbstractValidator<UserChangeEmailInfo>
     {
-        public UserUpdateProfileInfoValidator()
+        public UserChangeEmailInfoValidator()
         {
             var EmailAddressPattern = new EmailAddressAttribute();
             RuleFor(x => x.Email).Must(u => EmailAddressPattern.IsValid(u));
+            RuleFor(x => x.Password).NotEmpty().Length(6, 50);
+        }
+
+    }
+
+    public class UserChangePasswordInfoValidator : AbstractValidator<UserChangePasswordInfo>
+    {
+        public UserChangePasswordInfoValidator()
+        {
             RuleFor(x => x.Password).NotEmpty().Length(6, 50);
             RuleFor(x => x.RepeatedPassword).NotEmpty().Length(6, 50);
         }
 
     }
 }
+
+
 
